1ef6ee30156c028657541b93b550ab1da5eaa7b4
DESKTOP-G5L353K\takun <takunoshin123456789@gmail.com>
2021-09-08 20:42:07 +0900

    no message

--- Assets/Scripts/View/PlayerView.cs
+++ Assets/Scripts/View/PlayerView.cs
@@ -1,22 +1,6 @@
 ﻿
 using UnityEngine;
 
-<<<<<<< HEAD
-public class PlayerModel : CharacterModel
-{
-    PlayerModel model;
-    GameController controller;
-
-    
-    PlayerView view;    // プレイヤービュークラスの参照変数
-    public int shellSpeed = 10;
-    public int playerSpeed = 10;
-    public const float ShotWaitTime = 0f;
-    public const float MoveWaitTime = 0f;
-    private float waitTime = 0.2f;
-    private float countTime;
-=======
->>>>>>> 611a35310998388a0e33ad53c83ba6f01b3f7dfd
 
 public class PlayerView : CharacterView
 {
